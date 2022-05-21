dde01b288df3e362f00c3b67dc4daaf5457db6f7
devevh <70970042+devevh@users.noreply.github.com>
2022-05-21 16:24:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./gestion_bar_snack/js/sw.js').then(function(reg) {
+  navigator.serviceWorker.register('./js/sw.js').then(function(reg) {
 
     if(reg.installing) {
       console.log('Service worker installing');
