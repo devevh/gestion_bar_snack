1c1d34093d0420a96aabb15a1276b295a3039600
devevh <70970042+devevh@users.noreply.github.com>
2022-05-21 16:12:07 +0200

    Add files via upload
    optimisation de la fonction maj(), du manifeste, du serviceWorker

---
+++ app.js
@@ -0,0 +1,21 @@
+// register service worker
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./vente-bar-snack/js/sw.js').then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+else {
+	console.log('le navigateur ne supporte pas le serviceWorker');
+}
